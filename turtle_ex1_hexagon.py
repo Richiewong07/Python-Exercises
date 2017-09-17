ad988b52c18ee2ede2e242fc78810ef69f182d01
Richie Wong <richiewong@Richies-MacBook-Pro.local>
2017-09-16 20:11:07 -0500

    Updated

---
+++ turtle_ex1_hexagon.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+right(60)
+forward(100)
+
+
+
+mainloop()
