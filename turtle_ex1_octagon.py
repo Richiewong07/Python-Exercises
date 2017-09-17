0c3a53bdb7609e00a17ae2129f5d1bedd08b56f9
Richie Wong <richiewong@Richies-MacBook-Pro.local>
2017-09-16 20:11:33 -0500

    Updated

---
+++ turtle_ex1_octagon.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+
+forward(100)
+right(45)
+forward(100)
+right(45)
+forward(100)
+right(45)
+forward(100)
+right(45)
+forward(100)
+right(45)
+forward(100)
+right(45)
+forward(100)
+right(45)
+forward(100)
+
+
+
+mainloop()
