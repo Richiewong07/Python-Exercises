cc54fd68bdfc24e1bbd1f229ba3a5079826b1299
Richie Wong <richiewong@Richies-MacBook-Pro.local>
2017-09-17 17:00:38 -0500

    Week 1

--- madlib.py
+++ madlib.py
@@ -1,3 +1,6 @@
+# Prompt the user for the missing words to a Madlib sentence using the input
+# function. You will make up your own Madlib sentence
+
 name = input('What is name?')
 subject = input('What is subject?')
 print(name +"'s favorite subject in school is " + subject + ".")
