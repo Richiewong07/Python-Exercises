72b6533de99539e53aa03f112d7771e1532dbd72
Richie Wong <richiewong@Richies-MacBook-Pro.local>
2017-09-17 16:59:12 -0500

    Week 1

--- hello2.py
+++ hello2.py
@@ -1,3 +1,6 @@
+# Same as the first exercise, but this time print the user's name in ALL CAPS,
+# and also tell them the number of letters in their name.
+
 name = input('What is your name?')
 name = name.upper()
 letter = len(name)
