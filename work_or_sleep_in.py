5eba68594cb67eefbc6f587a7c693fe540f42c35
Richie Wong <richiewong@Richies-MacBook-Pro.local>
2017-09-17 17:06:00 -0500

    Week 1

--- work_or_sleep_in.py
+++ work_or_sleep_in.py
@@ -1,3 +1,7 @@
+# Prompt the user for a day of the week just like the previous problem.
+# Except this time print "Go to work" if it's a work day and "Sleep in" if it's
+# a weekend day.
+
 day = int(input('Day (0-6)'))
 if (day == 0 or day == 6):
     print ('Sleep in')
