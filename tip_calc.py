d59c537ddfda55d8d57963e70df0a598f58c29ee
Richie Wong <richiewong@Richies-MacBook-Pro.local>
2017-09-17 17:27:01 -0500

    Week 1

--- tip_calc.py
+++ tip_calc.py
@@ -1,3 +1,15 @@
+# Prompt the user for two things:
+#
+# The total bill amount:
+# The level of service, which can be one of the following: good, fair, or bad
+# Calculate the tip amount and the total amount (bill amount + tip amount).
+# The tip percentage based on the level of service is based on:
+#
+# good -> 20%
+# fair -> 15%
+# bad -> 10%
+
+
 total_bill = float(input('Total bill amount? '))
 service = input('Level of service? ')
 good = float(0.2 * total_bill)
