3b9d1759c18836ccce6603fdde50aadfe4cf528a
Richie Wong <richiewong@Richies-MacBook-Pro.local>
2017-09-17 17:03:50 -0500

    Week 1

--- day_of_week.py
+++ day_of_week.py
@@ -1,3 +1,10 @@
+# Given the following code that prompts the user for a day number
+# (the int function converts a numeric string to a number):
+
+# The user will enter a number between 0 to 6 inclusive.
+# Given this number, print a day of the week. 0 for Sunday, 1 for Monday, 2 for
+# Tuesday, and so on.
+
 day = int(input('Day (0-6)? '))
 if day == 0:
     print('Sunday')
