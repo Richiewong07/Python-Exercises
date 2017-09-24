597c7c6bb0d03268b285b9cf40ab2bcab8d5a667
Richie Wong <richiewong07@yahoo.com>
2017-09-24 16:03:12 -0500

    Updated

--- coins.py
+++ coins.py
@@ -1,4 +1,4 @@
-# Write a program that will prompt you for how many coins you want. 
+# Write a program that will prompt you for how many coins you want.
 # Initially you have no coins. It will ask you if you want a coin?
 # If you type "yes", it will give you one coin, and print out the current tally.
 # If you type no, it will stop the program.
