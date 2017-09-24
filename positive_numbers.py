987bc98fd367d4e3c2406b7670aa36a3d889c7e8
Richie Wong <richiewong07@yahoo.com>
2017-09-24 16:10:41 -0500

    Updated

--- positive_numbers.py
+++ positive_numbers.py
@@ -1,5 +1,8 @@
-numbers = [-1, -2, 0, 1, 2, 3, 4, 5]
+num_list1 = [10, 2, -4, 5, -3, 73]
+num_list2 = []
 
-for number in numbers:
-    if number > 0:
-        print(number)
+for i in num_list1:
+    if i > 0:
+        num_list2.append(i)
+
+print(num_list2)
