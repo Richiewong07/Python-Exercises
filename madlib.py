f9a284f28031ef549caa23f4331ce3392a2eadd1
Richie Wong <richiewong07@yahoo.com>
2017-09-24 13:58:00 -0500

    Updated

--- madlib.py
+++ madlib.py
@@ -2,5 +2,7 @@
 # function. You will make up your own Madlib sentence
 
 name = input('What is name?')
-subject = input('What is subject?')
-print(name +"'s favorite subject in school is " + subject + ".")
+hobby = input('What do you like to do?')
+color = input('What is your favorite color?')
+print("My name is {0}. I like to {1} in my free time. My favorite color is {2}."
+.format(name.lower(), hobby.lower(), color.lower()))
