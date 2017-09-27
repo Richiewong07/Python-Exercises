736fb9b5b7e0b56ef3418dc4e686b259434c7e82
Richie Wong <richiewong07@yahoo.com>
2017-09-27 09:02:41 -0500

    need to finish

--- algorithm_3.py
+++ algorithm_3.py
@@ -1,11 +1,18 @@
-sequence = list(range(1, 10))
-sequence_2 = []
-i = 1
+def fib():
+    x,y = 0,1
+    while True:
+        yield x
+        x,y = y, x+y
 
-for i in sequence:
-    j += 1
-    sequence_2.append(j)
+def even(seq):
+    for number in seq:
+        if not number %2:
+            yield number
 
+def under_a_million(seq):
+    for number in seq:
+        if number > 4000000:
+            break
+        yield number
 
-
-print(sequence_2)
+print sum(even(under_a_million(fib())))
