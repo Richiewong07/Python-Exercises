1fd6dd72f25b11c19841d4dd9fe58ffb428b1f2e
Richie Wong <richiewong07@yahoo.com>
2018-02-23 03:24:30 -0600

    updated

--- python-udemy/Notes/Statements/list_comprehensions.py
+++ python-udemy/Notes/Statements/list_comprehensions.py
@@ -19,5 +19,14 @@
 # print(fahrenheit)
 
 
-results = [x if x%2==0 else 'ODD' for x in range(0,11)]
-print(results)
+# results = [x if x%2==0 else 'ODD' for x in range(0,11)]
+# print(results)
+
+mylist = []
+for x in [2,4,6]:
+    for y in [1,10,100]:
+        mylist.append(x*y)
+
+mylist = [x*y for x in [2,4,6] for y in [1,10,1000]]
+
+print(mylist)
