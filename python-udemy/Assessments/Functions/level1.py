c1b21fa1319af3420a51383b90b954520c40284b
Richie Wong <richiewong07@yahoo.com>
2018-02-24 16:09:30 -0600

    updated

--- python-udemy/Assessments/Functions/level1.py
+++ python-udemy/Assessments/Functions/level1.py
@@ -1,10 +1,9 @@
 # OLD MACDONALD: Write a function that capitalizes the first and fourth letters of a name
 
 def old_macdonald(name):
-    if len(name) > 3:
-        return name[:3].capitalize() + name[3:].capitalize()
-    else:
-        return 'Name is too short!'
+    first_half = name[:3]
+    second_half = name[3:]
+    return first_half.capitalize() + second_half.capitalize()
 
 results = old_macdonald('macdonald')
 print(results)
@@ -13,7 +12,9 @@ print(results)
 # MASTER YODA: Given a sentence, return a sentence with the words reversed
 
 def master_yoda(text):
-    return ' '.join(text.split()[::-1])
+    wordlist = text.split()
+    reverse_word_list = wordlist[::-1]
+    return ' '.join(reverse_word_list)
 
 results = master_yoda('I am home')
 print(results)
