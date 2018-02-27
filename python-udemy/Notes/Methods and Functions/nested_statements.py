ad3718150e2f772d59da4cee4ea78505affb11d7
Richie Wong <richiewong07@yahoo.com>
2018-02-27 16:38:25 -0600

    updated

--- python-udemy/Notes/Methods and Functions/nested_statements.py
+++ python-udemy/Notes/Methods and Functions/nested_statements.py
@@ -10,3 +10,20 @@ def func(x):
 
 print(x)
 func(x)
+
+
+print(".....")
+
+
+x = 50
+
+def func(x):
+    print(f'X is {x}')
+
+    # Local Reassignment on a global variable!
+    x = 'NEW VALUE'
+    print(f'I JUST LOCALLY CHANGED X TO {x}')
+    return x
+
+x = func(x)
+print(x)
