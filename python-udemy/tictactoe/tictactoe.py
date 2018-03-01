96f23cf5d6f38e3f8a11ea9205de8e0beb3be0e7
Richie Wong <richiewong07@yahoo.com>
2018-02-28 20:36:42 -0600

    updated

--- python-udemy/tictactoe/tictactoe.py
+++ python-udemy/tictactoe/tictactoe.py
@@ -8,16 +8,12 @@ def display_board(board):
     print('--|---|--')
     print(board[1] + ' | ' + board[2] + ' | ' + board[3])
 
-test_board = [' '] * 10
-display_board(test_board)
-
-
 
 def player_input():
-
     '''
     OUTPUT = (Player 1 marker, Player 2 marker)
     '''
+
     marker = ''
 
     # KEEP ASKING PLAYER 1 TO CHOOSE X OR O
@@ -28,18 +24,10 @@ def player_input():
     if marker == 'X':
         return ('X', 'O')
     else:
-        return ('O', 'X')
-
-player1_marker, player2_marker = player_input()
-
 
 
 def place_marker(board, marker, position):
     board[position] = marker
-
-place_marker(test_board, '$', 8)
-display_board(test_board)
-
 
 
 def win_check(board, mark):
@@ -60,17 +48,12 @@ def win_check(board, mark):
     (board[7] == mark and board[5] == mark and board[3] == mark) or
     (board[9] == mark and board[5] == mark and board[1] == mark))
 
-display_board(test_board)
-win_check(test_board, 'X')
-
 
 
 import random
 
 def choose_first():
-
-    flip = random.ranint(0, 1)
-
+    flip = random.randint(0, 1)
     if flip == 0:
         return 'Player 1'
     else:
@@ -79,13 +62,10 @@ def choose_first():
 
 
 def space_check(board, position):
-
     return board[position] == ' '
 
 
-
 def full_board_check(board):
-
     for i in range(1, 10):
         if space_check(board, i):
             return False
@@ -95,17 +75,88 @@ def full_board_check(board):
 
 
 def player_choice(board):
-
     position = 0
-
-    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, postion):
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         position = int(input('Choose a position: (1-9)'))
-
     return position
 
 
 def replay():
-
     choice = input('Play again? Enter Yes or No')
-
     return choice == 'Yes'
+
+# WHILE LOOP TO KEEP RUNNING THE GAME
+print('Welcome to Tic Tac Toe')
+
+while True:
+    # PLAY THE GAME
+
+    # SET EVERYTHING UP (BOARD, WHO'S FIRST, CHOOSE MARKERS X, 0)
+    the_board = [' '] * 10
+    player1_marker, player2_marker = player_input()
+
+    turn = choose_first()
+    print(turn + ' will go first')
+
+    play_game = input('Ready to play? y or no?')
+
+    if play_game == 'y':
+        game_on = True
+    else:
+        game_on = False
+
+    # GAMEPLAY
+    while game_on:
+
+        # PLAYER 1 TURN
+        if turn == 'Player 1':
+
+            # SHOW THE BOARD
+            display_board(the_board)
+
+            # CHOOSE A position
+            position = player_choice(the_board)
+
+            # PLACE MARKER ON THE POSITION
+            place_marker(the_board, player1_marker, position)
+
+            # CHECK IF THEY WON
+            if win_check(the_board, player1_marker):
+                display_board(the_board)
+                print('PLAYER 1 HAS WON')
+                game_on = False
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('TIE GAME!')
+                    game_on = False
+                else:
+                    turn = 'Player 2'
+
+        # PLAYER 2 TURN
+        else:
+            # SHOW THE BOARD
+            display_board(the_board)
+
+            # CHOOSE A position
+            position = player_choice(the_board)
+
+            # PLACE MARKER ON THE POSITION
+            place_marker(the_board, player2_marker, position)
+
+            # CHECK IF THEY WON
+            if win_check(the_board, player2_marker):
+                display_board(the_board)
+                print('PLAYER 2 HAS WON')
+                game_on = False
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('TIE GAME!')
+                    game_on = False
+                else:
+                    turn = 'Player 1'
+
+    # BREAKOUT OF THE WHILE LOOP ON replay()
+    if not replay():
+        break
