3516769e9943d21e5d54f210eab394dba1133634
Richie Wong <richiewong07@yahoo.com>
2018-03-02 19:30:58 -0600

    updated

--- python-udemy/Assessments/Statements/listcomprehension.py
+++
@@ -1,5 +0,0 @@
-# Use List Comprehension to create a list of the first letters of every word in the string below:
-
-st = 'Create a list of the first letters of every word in this string'
-
-print([word[0] for word in st.split()])
