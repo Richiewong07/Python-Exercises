4f784b69a885776464f47a123238c4b5f6948a0a
Richie Wong <richiewong07@yahoo.com>
2018-03-03 01:24:48 -0600

    updated

--- python-udemy/Assessments & Challenges/Object Oriented Programming/oop_challenge.py
+++ python-udemy/Assessments & Challenges/Object Oriented Programming/oop_challenge.py
@@ -8,3 +8,14 @@
 
 # As an added requirement, withdrawals may not exceed the available balance.
 # Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
+
+class Account():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
