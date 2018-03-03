c1b7df94dcb20bc797a7009e214427578bac3121
Richie Wong <richiewong07@yahoo.com>
2018-03-02 20:09:52 -0600

    updated

--- python-udemy/Assessments & Challenges/Object Oriented Programming/object_oriented_programming.py
+++ python-udemy/Assessments & Challenges/Object Oriented Programming/object_oriented_programming.py
@@ -33,3 +33,26 @@ print('Slope: {}'.format(slope))
 
 # Problem 2
 # Fill in the class
+
+class Cylinder():
+    pi = 3.14
+
+    def __init__(self, height = 1, radius = 1):
+        self.height = height
+        self. radius = radius
+
+    def volume(self):
+        return Cylinder.pi * (self.radius ** 2) * self.height
+
+    def surface_area(self):
+        top = (Cylinder.pi * self.radius ** 2)
+        return (2 * top) + (2 * Cylinder.pi * self.radius * self.height)
+
+
+c = Cylinder(2,3)
+
+volume = c.volume()
+surface_area = c.surface_area()
+
+print('Volume: {}'.format(volume))
+print('Surface Area: {}'.format(surface_area))
