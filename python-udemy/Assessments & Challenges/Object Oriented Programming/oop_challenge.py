2765bd2f1cca27f744be97f3fe4c50613234947b
Richie Wong <richiewong07@yahoo.com>
2018-03-02 20:12:34 -0600

    updated

---
+++ python-udemy/Assessments & Challenges/Object Oriented Programming/oop_challenge.py
@@ -0,0 +1,10 @@
+# For this challenge, create a bank account class that has two attributes:
+    # owner
+    # balance
+
+# and two methods:
+    # deposit
+    # withdraw
+
+# As an added requirement, withdrawals may not exceed the available balance.
+# Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
