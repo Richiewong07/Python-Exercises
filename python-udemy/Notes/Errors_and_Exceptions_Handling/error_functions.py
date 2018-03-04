98d0f3e2a5b893074a73a7a5b1f18f02aded8d98
Richie Wong <richiewong07@yahoo.com>
2018-03-04 17:35:56 -0600

    updated

--- python-udemy/Notes/Errors_and_Exceptions_Handling/error_functions.py
+++ python-udemy/Notes/Errors_and_Exceptions_Handling/error_functions.py
@@ -1,9 +1,14 @@
 def ask_for_int():
-    try:
-        result = int(input('Please provide number: '))
-    except:
-        print('Whoops! That is not a number')
-    finally:
-        print('End of try/excpet/finally')
+    while True:
+        try:
+            result = int(input('Please provide number: '))
+        except:
+            print('Whoops! That is not a number')
+            continue
+        else:
+            print('Yes thank you')
+            break
+        # finally:
+        #     print("I'm going to ask you again! \n")
 
 ask_for_int()
