8605a665c3deb96da4c9873a1d9d80379ce43848
Richie Wong <richiewong07@yahoo.com>
2018-03-04 19:04:12 -0600

    updated

---
+++ python-udemy/Assessments_and_Challenges/Error_and_Exceptions/problem_1.py
@@ -0,0 +1,11 @@
+# Problem 1
+# Handle the exception thrown by the code below by using try and except blocks.
+
+# for i in ['a', 'b' 'c']:
+#     print(i ** 2)
+
+try:
+    for i in ['a', 'b', 'c']:
+        print (i ** 2)
+except:
+    print('General Error!' Watch out!)
