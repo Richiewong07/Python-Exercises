8bf28a153de9e7a186454b77fc351134c3d28d21
Richie Wong <richiewong07@yahoo.com>
2018-03-08 21:11:27 -0600

    updated

--- python-assignments/python-part1/1_to_10.py
+++ python-assignments/python-part1/1_to_10.py
@@ -1,6 +1,7 @@
 # Use a while loop to print the numbers from 1 to 10.
 
-count = 0
-while count < 10:
-    count += 1
-    print(count)
+def count():
+    count = 0
+    while count < 10:
+        count += 1
+        print(count)
