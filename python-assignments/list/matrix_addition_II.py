2e40c577256c2ae6a9c1c2111b22443e439b1750
Richie Wong <richiewong07@yahoo.com>
2018-03-11 22:27:03 -0500

    updated

--- python-assignments/list/matrix_addition_II.py
+++ python-assignments/list/matrix_addition_II.py
@@ -1,9 +1,23 @@
 
-answer = []
+# answer = []
+#
+# for i in range(0, len(a)):
+#     row = []
+#     for j in range(0, len(a[0])):
+#         row.append(a[i][j]+ b[i][j])
+#     answer.append(row)
+# print(answer)
 
-for i in range(0, len(a)):
-    row = []
-    for j in range(0, len(a[0])):
-        row.append(a[i][j]+ b[i][j])
-    answer.append(row)
-print(answer)
+
+m1 = [[1, 3], [2, 4]]
+m2 = [[5, 2], [1, 0]]
+
+def matrix_add(a, b):
+
+    for i in range(0, len(a)):
+        row = []
+        for j in range(0, len(a[0])):
+            row.append(a[i][j] + b[i][j])
+        print(row)
+
+matrix_add(m1, m2)
