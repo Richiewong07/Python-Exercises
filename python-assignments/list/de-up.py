6f1927967a05d11e39a1d29916666fdb0cdafbe2
Richie Wong <richiewong07@yahoo.com>
2018-03-12 18:09:31 -0500

    updated

--- python-assignments/list/de-up.py
+++ python-assignments/list/de-up.py
@@ -1,9 +1,15 @@
+# 11. De-dup
+# Given an list of numbers or strings, create a new list containing the same elements as the first list, except with any duplicate values removed. Print the list.
+
+
 mylist = [0, 1, 1, 2, 3, 4, 4, 4, 5]
 
-newlist = []
+def duplicate(list):
+    newlist = []
+    for num in list:
+        if num not in newlist:
+            newlist.append(num)
+    return newlist
 
-for number in mylist:
-    if number not in newlist:
-        newlist.append(number)
-
-print(newlist)
+result = duplicate(mylist)
+print(result)
