9c48cff7cabae20e2483dc9330fd11acd16818b4
Richie Wong <richiewong07@yahoo.com>
2018-03-13 19:16:54 -0500

    updated

--- python-assignments/string/casesar_cipher.py
+++ python-assignments/string/casesar_cipher.py
@@ -1,2 +1,7 @@
+# 6. Caesar Cipher
+# Given a string, print the Caesar Cipher (or ROT13) of that string. What is Caesar Cipher? http://practicalcryptography.com/ciphers/caesar-cipher/
+#
+# Use your solution to decipher the following text: "lbh zhfg hayrnea jung lbh unir yrnearq"
+
 text = 'lbh zhfg hayrnea jung lbh unir yrnearq'
 ROT = 13
