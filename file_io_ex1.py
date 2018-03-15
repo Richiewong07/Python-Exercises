e08cb004f046deffbdddd682367e2083ad50e680
Richie Wong <richiewong07@yahoo.com>
2018-03-14 20:12:06 -0500

    updated

--- file_io_ex1.py
+++
@@ -1,9 +0,0 @@
-# Write a program that prompts the user to enter a file name,
-# reads the contents of the file and prints it to the screen.
-
-file_name = input("Enter a file name here")
-file_handle = open(file_name , 'r')
-contents = file_handle.read()
-file_handle.close()
-
-print(contents)
