40e370a18aadb545832c41ae1b8e9231608454f8
Richie Wong <richiewong07@yahoo.com>
2018-03-16 03:44:30 -0500

    updated

--- python-assignments/phone-book-app/phone-book-app.py
+++ python-assignments/phone-book-app/phone-book-app.py
@@ -1,6 +1,8 @@
 import json
 
 
+# 1. If they choose to look up an entry, you will ask them for the person's name, and then look up the person's phone number by the given name and print it to the screen.
+
 def look_up():
     name = input("What is the person's name: ").capitalize()
 
@@ -9,6 +11,37 @@ def look_up():
         # print(contacts[name])
         print(contacts.get(name, 'Name not found'))
 
+# 2. If they choose to set an entry, you will prompt them for the person's name and the person's phone number,
+
+def set_entry():
+
+    contacts = {}
+
+    name = input("Enter name: ").capitalize()
+    contacts[name] = {"phone": None, "email": None, "URL": None}
+
+    phone = input("Enter phone number (XXX-XXX-XXX): ")
+    contacts[name]['phone'] = phone
+    # email = input("Enter email: ")
+    # url = input("Enter URL")
+
+    with open('phone_book.json', 'w') as file_handle:
+        json.dump(contacts, file_handle)
+
+# 3. If they choose to delete an entry, you will prompt them for the person's name and delete the given person's entry.
+
+def delete_entry():
+    pass
+
+# 4. If they choose to list all entries, you will go through all entries in the dictionary and print each out to the terminal.
+
+def list_all():
+    pass
+
+# 5. If they choose to quit, end the program.
+
+def quit():
+    pass
 
 
 
@@ -18,11 +51,12 @@ def main():
 
     user_input = int(input('Enter Number (1-5): '))
 
+
     while True:
         if user_input == 1:
             look_up()
         elif user_input == 2:
-            print(2)
+            set_entry()
         elif user_input == 3:
             print(3)
         elif user_input == 4:
