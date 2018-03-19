79a79cef78c082e32d5391b1d55d19f516668546
Richie Wong <richiewong07@yahoo.com>
2018-03-19 16:27:32 -0500

    updated

---
+++ Notes/objects/3_classmethods_and_staticmethods.py
@@ -0,0 +1,58 @@
+class Employee:
+
+    raise_amount = 1.04
+    num_of_emps = 0
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    # CLASS METHOD
+    # NEED TO ADD DECORATOR
+    # NEED TO PASS IN cls
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    # CLASS METHOD AS ALTERNATIVE CONSTRUCTOR
+    # USUALLY STARTS WITH FROM
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+
+        # USE CLASS VARIABLE --> CREATES NEW EMPLOYEE
+        return cls(first, last, pay)
+
+
+emp_1 = Employee('Richie', 'Wong', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+
+# HOW TO USE CLASS METHOD
+Employee.set_raise_amount(1.05) # --> SAME AS Employee.set_raise_amount(1.05)
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_1.raise_amount)
+
+
+# CLASS METHODS AS ALTERNATIVE CONSTRUCTORS --> USE CLASS METHODS TO PROVIDE MULTIPLE WAYS OF CREATING OUR OBJECTS
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
