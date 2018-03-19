d4eb4e85a05ece8d8e858a8d2377b0f2114b5b13
Richie Wong <richiewong07@yahoo.com>
2018-03-19 18:45:51 -0500

    updated

--- Notes/objects/3_classmethods_and_staticmethods.py
+++ Notes/objects/3_classmethods_and_staticmethods.py
@@ -19,13 +19,13 @@ class Employee:
 
     # CLASS METHOD
     # NEED TO ADD DECORATOR
-    # NEED TO PASS IN cls
+    # AUTOMATICALLY PASS cls
     @classmethod
     def set_raise_amount(cls, amount):
         cls.raise_amount = amount
 
     # CLASS METHOD AS ALTERNATIVE CONSTRUCTOR
-    # USUALLY STARTS WITH FROM
+    # USUALLY STARTS WITH from
     @classmethod
     def from_string(cls, emp_str):
         first, last, pay = emp_str.split('-')
@@ -33,6 +33,14 @@ class Employee:
         # USE CLASS VARIABLE --> CREATES NEW EMPLOYEE
         return cls(first, last, pay)
 
+    # STATIC METHODS
+    # DOES NOT AUTOMATICALLY PASS ANYTHING
+    # DOES NOT DEPEND ON ANY INSTANCE OR CLASS VARIABLES
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 emp_1 = Employee('Richie', 'Wong', 50000)
 emp_2 = Employee('Test', 'User', 60000)
@@ -56,3 +64,10 @@ new_emp_1 = Employee.from_string(emp_str_1)
 
 print(new_emp_1.email)
 print(new_emp_1.pay)
+
+# USE OF STATIC METHODS
+import datetime
+
+my_date = datetime.date(2016, 7, 10)
+
+print(Employee.is_workday(my_date))
