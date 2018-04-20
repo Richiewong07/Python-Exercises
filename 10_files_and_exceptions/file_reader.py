d67bdbdb250251c7aaf16f57508f19dca88dd983
Richie Wong <richiewong07@yahoo.com>
2018-04-20 17:45:39 -0500

    updated

--- 10_files_and_exceptions/file_reader.py
+++ 10_files_and_exceptions/file_reader.py
@@ -1,5 +1,5 @@
 # READING ENTIRE FILES
-with open('pi_digits.txt') as file_object:
+with open('text_files/pi_digits.txt') as file_object:
     contents = file_object.read()
     print(contents)
 
@@ -18,16 +18,16 @@ with open(file_path) as file_object:
 
 
 # READING LINE BY LINE
-file_name = 'pi_digits.txt'
-with open(file_name) as file_objects:
-    for line in file_objects:
+file_name = 'text_files/pi_digits.txt'      # STORE NAME OF FILE
+with open(file_name) as file_objects:       # OPEN/CLOSE FILE PROPERLY
+    for line in file_objects:               # LOOP THROUGH EACH LINE IN FILE OBJECT
         print(line.rstrip())
 
 
 # MAKING A LIST OF LINES FROM A FILE
-file_name = 'pi_digits.txt'
+file_name = 'text_files/pi_digits.txt'
 with open(file_name) as file_objects:
     lines = file_objects.readlines()        # ALLOWS ACCESS TO FILE OBJECT OUTSIDE OF BLOCK, readlines() --> stores lines in a list
 
-for line in lines:
+for line in lines:                          # FOR LOOP TO PRINT EACH LINE
     print(line.rstrip())
