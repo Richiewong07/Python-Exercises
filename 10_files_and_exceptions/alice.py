cd055b6c35b6573af6ff06ff89c1d7efc64c62b8
Richie Wong <richiewong07@yahoo.com>
2018-04-22 20:59:27 -0500

    updated

--- 10_files_and_exceptions/alice.py
+++ 10_files_and_exceptions/alice.py
@@ -1,8 +1,13 @@
-filename = "alice.txt"
+filename = "text_files/alice.txt"
+
 
 try:
-    with open(filename) as f_objects:
-        contents = f_objects.read()
+    with open(filename) as f_obj:
+        contents = f_obj.read()
 except FileNotFoundError:
     msg = "Sorry, the file " + filename + " does not exists."
     print(msg)
+else:
+    words = contents.split()    # SPLIT ALL THE WORDS IN FILE INTO A LIST
+    num_words = len(words)      # GET LENGTH OF LIST
+    print("The file " + filename + " has about " + str(num_words) + " words.")
