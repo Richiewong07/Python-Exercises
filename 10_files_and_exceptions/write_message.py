e33cf346dc81ef819f0764f697c98f51af0b1b29
Richie Wong <richiewong07@yahoo.com>
2018-04-22 20:19:36 -0500

    updated

--- 10_files_and_exceptions/write_message.py
+++ 10_files_and_exceptions/write_message.py
@@ -1,4 +1,5 @@
 filename = 'text_files/programming.txt'
 
-with open(filename, 'w') as file_object:
-    file_object.write('I love programming.')
+with open(filename, 'w') as file_object:    # OPEN FILE IN WRITE MODE
+    file_object.write('I love programming.\n')
+    file_object.write('I love createing new games.\n')
