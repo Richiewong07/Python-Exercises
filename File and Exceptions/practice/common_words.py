92f50f5f2e2a9f7ce62fdff915db8ae48754c3dc
Richie Wong <richiewong07@yahoo.com>
2018-04-25 16:32:57 -0500

    updated

--- File and Exceptions/practice/common_words.py
+++
@@ -1,7 +0,0 @@
-
-filename = 'row.txt'
-
-with open(filename) as file_object:
-    content = file_object.read()
-    common_words = content.lower().count('row')
-    print('The word row appears {} in the song.'.format(common_words))
--- testing/name_function.py
+++
@@ -1,4 +0,0 @@
-def get_formatted_name(first, middle, last):
-    """GENERATE A NEATLY FORMATTED FULL NAME."""
-    full_name = first + ' ' + middle + ' ' + last
-    return full_name.title()
