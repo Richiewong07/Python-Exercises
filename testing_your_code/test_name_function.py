2d736f89d0314783ebc9e44208726f0abb80f34f
Richie Wong <richiewong07@yahoo.com>
2018-04-25 17:42:41 -0500

    updated

--- testing_your_code/test_name_function.py
+++ testing_your_code/test_name_function.py
@@ -9,6 +9,11 @@ class NameTestCase(unittest.TestCase):  # CREATE A CLASS THAT CONTAINS A SERIES 
         formatted_name = get_formatted_name('janis', 'joplin')  # CALL FUNCTION YOU WANT TO TEST AND STORE A RETURN VALUE FOR TESTING
         self.assertEqual(formatted_name, 'Janis Joplin')    # ASSERT METTHOD --> VERIFIES THAT THE RESULT YOU RECEIVED MATCHES THE RESULT YOU EXPECTED TO RECEIVE
 
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 unittest.main()
 
 # "." ON FIRST LINE --> TELLS US SINGLE TEST IS PASSED
