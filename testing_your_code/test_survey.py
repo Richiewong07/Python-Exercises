788cf971891ed169c079ca2e3fd1e317a25eeb44
Richie Wong <richiewong07@yahoo.com>
2018-04-25 20:36:35 -0500

    updated

--- testing_your_code/test_survey.py
+++ testing_your_code/test_survey.py
@@ -4,24 +4,26 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     """Test for the class AnonymousSurvey"""
 
+    def setUp(self):
+        """Create a survey and a set of responses for use in all test methods."""
+        question = "What language did your first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)  # SETUP CREATES A SURVEY INSTANCE AND CREATES A LIST OF RESPONSES
+        self.responses = ['English', 'Spanish', 'Mandarin']
+
     def test_store_single_response(self):
         """Test that a single response is stored properly."""
-        question = "What language did your first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
+        self.my_survey.store_response(self.responses[0])
 
-        self.assertIn('English', my_survey.responses)
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_three_response(self):
         """Test that three individual responses are stored properly."""
-        question = "What language did your first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Spanish', 'Mandarin']
-        for response in responses:
-            my_survey.store_response(response)
 
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 
 unittest.main()
