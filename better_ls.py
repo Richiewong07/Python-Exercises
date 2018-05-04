878bb00c7179212ff66d0ba3683bbcfe0fdf2b31
Richie Wong <richiewong07@yahoo.com>
2018-05-03 20:10:29 -0500

    updated

--- better_ls.py
+++
@@ -1,8 +0,0 @@
-import ls
-
-def better():
-    print("I'm Awesome")
-    ls.ls()
-
-if __name__ == "__main__":
-    better()
--- ls.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def ls():
-    print(os.listdir())
-
-if__name__ == '__main__'
-    ls()
